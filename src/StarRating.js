83abd510655a13fdc0785f07e77654afadfe59a0
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2023-08-09 13:50:42 +1000

    Creating reusable star rating component

---
+++ src/StarRating.js
@@ -0,0 +1,30 @@
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+
+function StarRating({ maxRating = 5 }) {
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p style={textStyle}>{maxRating}</p>
+    </div>
+  );
+}
+
+export default StarRating;
